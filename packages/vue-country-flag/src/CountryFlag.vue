d1d874c1bb660ac2a32e9940aafa6041e536e924
P3trur0 <ubaldopescatore@gmail.com>
2021-11-23 10:52:36 +0100

    margin fixed for small and normal flags

--- packages/vue-country-flag/src/CountryFlag.vue
+++ packages/vue-country-flag/src/CountryFlag.vue
@@ -65,12 +65,14 @@ export default {
 </script>
 <style scoped>
   .small-flag {
+    margin: -0.9em -1.2em -0.9em -1.2em;
     transform: scale(0.25);
     -ms-transform: scale(0.25); 
     -webkit-transform: scale(0.25);
     -moz-transform: scale(0.25);
   }
   .normal-flag {
+    margin: 0em -0.9em -0.6em -0.7em;
     transform: scale(0.5);
     -ms-transform: scale(0.5); 
     -webkit-transform: scale(0.5);
